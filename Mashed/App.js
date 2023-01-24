476d3f65750cf69b141632be7d00bc780190f73a
naomy <92695534+nnoguez@users.noreply.github.com>
2023-01-23 22:16:50 -0500

    set up

---
+++ Mashed/App.js
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to Mashed!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+// styling 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
